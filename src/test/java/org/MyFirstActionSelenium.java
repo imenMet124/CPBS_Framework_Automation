892a15c1bd55ff0b78db71cd7ce03b814ee68cf0
imenMet124 <methniimen@gmail.com>
2024-02-28 17:40:44 +0100

    first test selenium

---
+++ src/test/java/org/MyFirstActionSelenium.java
@@ -0,0 +1,9 @@
+package org;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.*;
+
+public class MyFirstActionSelenium {
+
+}
