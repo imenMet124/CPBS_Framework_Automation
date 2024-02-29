fe834e35f82d973028f1c4ada511c4c78ce95b6a
imenMet124 <methniimen@gmail.com>
2024-02-29 16:54:52 +0100

    second

--- src/test/java/org/MyFirstActionSelenium.java
+++ src/test/java/org/MyFirstActionSelenium.java
@@ -1,9 +1,22 @@
 package org;
-import org.junit.*;
-import org.openqa.selenium.*;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.junit.jupiter.api.*;
 
 public class MyFirstActionSelenium {
+    public static void main(String[] args) {
+        // Spécifiez l'emplacement du chromedriver.exe
+        System.setProperty("webdriver.chrome.driver", "C:\\CPBS_Framework_Automation\\drivers\\chromedriver.exe");
 
+        // Initialisez le WebDriver pour Chrome
+        WebDriver driver = new ChromeDriver();
+
+        // Naviguer vers l'URL de votre site web
+        driver.get("https://www.boulanger.com/");
+
+        // Deletes all the cookies
+        driver.manage().deleteAllCookies();
+
+        // Fermer le navigateur
+        driver.quit();
+    }
 }
